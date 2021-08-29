7e779d85af6126c21abd5ef3e94c963c2d2303d8
Maria Rita Severino <mariarsseverino@gmail.com>
2021-08-29 19:59:51 -0300

    Add files via upload

---
+++ ClasseA.py
@@ -0,0 +1,22 @@
+class ClasseA:
+    def __init__(self, A1, A2):
+        self.A1 = A1
+        self.A2 = A2
+
+    def setA1(self, A1):
+        self.A1 = A1
+
+    def setA2(self, A2):
+        self.A2 = A2
+
+    def getA1(self, A1):
+        self.A1 = A1
+
+    def getA2(self, A2):
+        self.A2 = A2
+
+    def MA1(self):
+        print("MA1")
+
+    def MA2(self):
+        print("MA2")
---
+++ ClasseB.py
@@ -0,0 +1,22 @@
+class ClasseB:
+    def __init__(self, B1, B2):
+        self.B1 = B1
+        self.B2 = B2
+
+    def setB1(self, B1):
+        self.B1 = B1
+
+    def setB2(self, B2):
+        self.B2 = B2
+
+    def getB1(self, B1):
+        self.B1 = B1
+
+    def getB2(self, B2):
+        self.B2 = B2
+
+    def MB1(self):
+        print("MB1")
+
+    def MB2(self):
+        print("MB2")
